bef7fa9ccbd4c3836007c36f56ca35711923f556
Michael Pumo <hello@michaelpumo.com>
2025-10-29 14:59:52 +0000

    Custom viewports.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,30 @@
 /** @type { import('@storybook/html-vite').Preview } */
+
+import { INITIAL_VIEWPORTS } from 'storybook/viewport';
+
+const alpitronicViewports = {
+  main: {
+    name: 'HYC150 to HYC400',
+    styles: {
+      width: '1366px',
+      height: '768px',
+    },
+    type: 'desktop',
+  },
+};
+
 const preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    viewport: {
+      options: {
+        ...alpitronicViewports,
+        ...INITIAL_VIEWPORTS,
       },
     },
   },
