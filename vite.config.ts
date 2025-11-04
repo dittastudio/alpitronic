c7437b9f29f4018e00274e54a519886cd732c0b8
Michael Pumo <hello@michaelpumo.com>
2025-11-04 17:42:34 +0000

    Tidy up.

--- vite.config.ts
+++ vite.config.ts
@@ -16,21 +16,12 @@ function getComponentEntries() {
       const fullPath = resolve(dir, item);
       const stat = statSync(fullPath);
 
-      // console.log('fullPath: ', fullPath);
-      // console.log('stat (isDirectory): ', stat.isDirectory());
-
       if (stat.isDirectory()) {
-        // console.log('item: ', item);
         const htmlFile = resolve(fullPath, `${item}.html`);
-
-        console.log('htmlFile: ', htmlFile);
 
         try {
           statSync(htmlFile);
           const entryName = basePath ? `${basePath}/${item}` : item;
-
-          console.log('entryName: ', entryName);
-
           entries[entryName] = htmlFile;
         } catch {
           scanDirectory(fullPath, basePath ? `${basePath}/${item}` : item);
@@ -80,38 +71,23 @@ export default defineConfig({
           return 'assets/[name]-[hash].js';
         },
         assetFileNames: assetInfo => {
-          const name = assetInfo.name || '';
-
-          if (name.endsWith('.html')) {
-            for (const entry in getComponentEntries()) {
-              if (entry !== 'index' && name.includes(`/${entry}/`)) {
-                return `${entry}/[name][extname]`;
-              }
-            }
-            return '[name][extname]';
-          }
+          const name = assetInfo.names[0] || '';
 
           for (const entry in getComponentEntries()) {
-            // console.log('LOOPING: getComponentEntries');
             if (entry !== 'index' && name.includes(`/${entry}/`)) {
-              // console.log('ENTRY:', entry, name);
               return `${entry}/[name]-[hash][extname]`;
             }
           }
-
-          // console.log('OTHER:', name);
 
           const fileParts = name.split('.');
           const fileName = fileParts.at(0);
           const fileExt = fileParts.at(-1);
 
           if (fileName && fileExt && fileExt === 'css') {
-            // console.log('WE HAVE CSS', fileName, fileExt);
             return `${fileName}/[name]-[hash][extname]`;
           }
 
           return 'assets/[name]-[hash][extname]';
-          // return `${entry}/[name]-[hash][extname]`
         },
       },
     },
