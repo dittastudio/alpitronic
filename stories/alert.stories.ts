6d646596105d0e5c0ffb1a8deabcd7da1d80d061
Michael Pumo <hello@michaelpumo.com>
2025-11-06 12:09:57 +0000

    Refactor alert.

--- stories/alert.stories.ts
+++ stories/alert.stories.ts
@@ -1,30 +1,28 @@
-import '@/css/app.css';
 import '@/components/alert/alert.css';
 import template from '@/components/alert/alert.html?raw';
 
 export default {
   title: 'Alpitronic/Alert',
   tags: ['autodocs'],
-  render: ({ type = 'success' }: { type?: 'success' | 'error' }) => {
+  render: ({ type }: { type?: 'success' | 'error' }) => {
     const wrapper = document.createElement('div');
     wrapper.innerHTML = template;
 
-    const alert: HTMLElement | null = wrapper.querySelector('.alert');
+    const alert = wrapper.firstChild as HTMLElement;
 
-    if (!alert) {
-      return wrapper;
-    }
+    // if (type === 'success') {
+    //   alert.classList.add('alert--success');
+    // } else {
+    //   alert.classList.add('alert--error');
+    // }
 
-    if (type === 'success') {
-      alert.classList.add('alert--success');
-    } else {
-      alert.classList.add('alert--error');
-    }
-
-    return wrapper.firstChild;
+    return alert;
   },
   argTypes: {
     type: { control: 'radio', options: ['success', 'error'] },
+  },
+  args: {
+    type: 'success',
   },
 };
 
